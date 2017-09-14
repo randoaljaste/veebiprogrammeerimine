84bc670f78cdcb2fd7f2d8041299da97088e4b51
Rando Aljaste <randoaljaste@users.noreply.github.com>
2017-09-14 10:47:13 +0300

    Lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Rando programmeerib veebi</title>
+</head>
+<body>
+<body style="background-color:powderblue;">
+	<h1>Rando Aljaste, veebiprogrammeerimine</h1>
+	<p>See veebileht on loodud õppetööraames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<?php
+		echo "Algas PHP õppimine.";
+	?>
+
+
+
+</body>
+</html>
