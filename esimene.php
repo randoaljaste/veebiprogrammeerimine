cf6aecb4389a6d4e76f27b57b8ef5f85b7aa47e6
Rando Aljaste <randoaljaste@users.noreply.github.com>
2017-09-21 10:24:08 +0300

    Lisatud massiiv, for tsükkel ja esimene vorm

--- esimene.php
+++ esimene.php
@@ -2,8 +2,12 @@
 	//muutujad
 	$myName = "Rando";
 	$myFamilyName = "Aljaste";
+	$monthNamesEt = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+	//var_dump($monthNamesEt);
+	//echo $monthNamesEt[8];
+	$monthNow = $monthNamesEt[date("n") - 1];
 	
-	//hindan päeva osa 
+	//hindan päeva osa 	võrdlemine  < > <= >= == != 
 	$hourNow = date("H");
 	$partOfDay = "";
 	if ($hourNow < 8){
@@ -16,6 +20,31 @@
 		$partOfDay = "vaba aeg";
 	}
 	//echo $partOfDay
+	
+	//vanusega tegelemine
+	//var_dump ($_POST)[birthYear];
+	//echo $_POST["birthYear"];
+	$myBirthYear;
+	$ageNotice = "";
+	if ( isset($_POST["birthYear"]) and $_POST["birthYear"] != 0){
+		$myBirthYear = $_POST["birthYear"];
+		$myAge = date("Y") - $_POST["birthYear"];
+		$ageNotice = "<p>Te olete umbkaudu " .$myAge ." aastat vana.</p>";
+		
+		$ageNotice .= "<p>Olete elanud järgnevatel aastatel:</p> <ul>";
+		for ($i = $myBirthYear; $i <= date("Y"); $i ++){
+			$ageNotice .= "<li>" .$i ."</li>";
+			
+		}
+		$ageNotice .= "</ol>";
+	}
+	
+	/*for ($i = 0; $i < 5; $i ++){
+		echo "ha";	
+	}*/
+	
+	
+	
 ?>
 
 
@@ -26,18 +55,30 @@
 	<title>Rando programmeerib veebi</title>
 </head>
 <body>
-<body style="background-color:powderblue;">
+<body style="background-color:lightgreen;">
 	<h1><?php echo $myName ." " .$myFamilyName; ?>, veebiprogrammeerimine</h1>
 	<p>See veebileht on loodud õppetööraames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
 	<?php
 		echo "<p>Algas PHP õppimine.</p>";
 		echo "<p>Täna on ";
-		echo date("d.m.Y.") .", kell oli lehe avamise hetkel " .date("H:i:s");
+		echo date("d. ") .$monthNow . date(" Y") .", kell oli lehe avamise hetkel " .date("H:i:s");
 		echo ", hetkel on " .$partOfDay .".</p>";
 		
 	?>
-
-
+	<h2>Natuke vanusest.</h2>
+	<form method="POST">
+		<label>Teie sünniaasta: </label>
+		<input name="birthYear" id="birthYear" type="number" value="<?php echo $myBirthYear; ?>" min="1900"  max="2017">
+		<input name="submitBirthYear" type="submit" value="Sisesta">
+	</form>
+	<?php
+		if ($ageNotice != ""){
+			echo $ageNotice;
+		}
+			
+	?>
+	
+	
 
 </body>
 </html>
